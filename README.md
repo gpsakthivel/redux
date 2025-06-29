69d6d5a808c5b87db1d91a80020240c0d45eda76
gpsakthivel <psakthi24vel@gmail.com>
2025-06-29 11:57:55 +0530

    Adding base proj

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # redux
+
+- [crud](https://www.youtube.com/watch?v=QgK_-G-hWeA)
+- [Redux Toolkit](https://www.youtube.com/playlist?list=PLC3y8-rFHvwiaOAuTtVXittwybYIorRB3)
---
+++ redux-toolkit-crud/src/components/MovieInput.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addMovie } from "../movieSlice";
+import { useState } from "react";
+
+export const MovieInput = () => {
+    const [newMovie, setNewMovie] = useState("");
+
+    const dispatch = useDispatch();
+
+    const handleAddMovie = () => {
+        if(newMovie) {
+            dispatch(addMovie(newMovie));
+            setNewMovie("");
+        }
+    }
+
+    return (
+        <>
+            <input onChange={(e) => setNewMovie(e.target.value)} value={newMovie} />
+            <button onClick={handleAddMovie}> Add Movie </button>
+        </>
+    );
+}
